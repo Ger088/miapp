7824b3a5449bf0d39e530f0c9c143a6e6bd81b42
Ger088 <100445716+Ger088@users.noreply.github.com>
2022-07-13 18:26:54 -0300

    Acerca de mi

--- miapp/src/app/app.module.ts
+++ miapp/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoArgentinaComponent } from './components/logo-argentina/logo-argentina.component';
 import { RedesComponent } from './components/redes/redes.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     LogoArgentinaComponent,
     RedesComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
